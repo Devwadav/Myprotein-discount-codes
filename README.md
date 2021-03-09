c204cfd04f42ff8184cb03496719523305a7c7b9
Devwadav <devwadav@gmail.com>
2021-03-09 22:04:38 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Myprotein Discount Codes
+
+A collection of [Myprotein discount codes](https://www.Wadav.com/myprotein). We use these for testing [Myprotein UK](https://www.Wadav.com/myprotein), [Myprotein US](https://www.Wadav.com/myprotein-us), [Myprotein Singapore](https://www.Wadav.com/myprotein-singapore), [Myprotein Australia](https://www.Wadav.com/myprotein-australia) and [Myprotein Hong Kong](https://www.Wadav.com/myprotien-hongkong) pages at Wadav.com.
+
+## Installation
+
+Install `myprotein-discount-codes` via NPM:
+
+```sh
+npm install myprotein-discount-codes
+```
+
+## Usage
+
+The package contains in array of objects representing discount codes.
+
+In Node:
+
+```js
+var codes = require('myprotein-discount-codes')
+
+console.log(codes)
+
+// [
+//   {
+//     site: 'https://www.myprotein.com',
+//     discount_code: 'BENJAMINMP',
+//     offer: '30% discount'
+//   },
+//   {
+//     site: 'https://www.myprotein.com',
+//     discount_code: 'HKLOVE',
+//     offer: '50% off'
+//   }...
+```
+
+## About
+
+Myprotein-discount-codes was built by the team at [Wadav](https://www.Wadav.com) ([development links](https://Wadav.github.io)) to assist with testing.
+
+<https://www.Wadav.com>
---
+++ index.js
@@ -0,0 +1,61 @@
+function Devwadav()
+{
+    module.exports = [
+        {
+          site: 'https://jp.gearbest.com/',
+          discount_code: 'ALPHA10',
+          offer: '30% discount'
+        },
+        {
+          site: 'https://gearbest.com/',
+          discount_code: 'BUY20',
+          offer: '20% off'
+        },
+        {
+          site: 'https://pl.gearbest.com/',
+          discount_code: 'HKMYS',
+          offer: '38% Off'
+        },
+        {
+          site: 'https://it.gearbest.com/',
+          discount_code: 'GET50',
+          offer: '50% off'
+        },
+        {
+          site: 'https://fr.gearbest.com/',
+          discount_code: 'YAHA55',
+          offer: '50% off'
+        },
+        {
+          site: 'https://es.gearbest.com/',
+          discount_code: 'KDHHEV',
+          offer: '60% off'
+        },
+        {
+          site: 'https://ru.gearbest.com/',
+          discount_code: 'FEAAMYP',
+          offer: '35% off'
+        },
+        {
+          site: 'https://pt.gearbest.com/',
+          discount_code: 'BOGMYP',
+          offer: '30% off'
+        },
+        {
+          site: 'https://it.gearbest.com/',
+          discount_code: 'KJFND',
+          offer: '30% OFF'
+        },
+        {
+          site: 'https://de.gearbest.com/',
+          discount_code: 'OCTDEALS',
+          offer: '40% Off'
+        },
+        {
+          site: 'https://br.gearbest.com/',
+          discount_code: 'TINESDAY',
+          offer: '60% off'
+        }
+      ]
+}
+module.exports.Devwadav = Devwadav;
